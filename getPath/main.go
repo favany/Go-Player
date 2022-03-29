6a6704e7ba885fd5c5183578a286c02ee1d8a542
Bingo <faaa@live.com>
2022-03-29 15:48:53 +0800

    🍐Gin 获取路径

--- getPath/main.go
+++ getPath/main.go
@@ -1,1 +1,33 @@
-package getPath
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	//r.GET("/:name/:age", func(c *gin.Context) {
+	//	// 获取路径参数
+	//	name := c.Param("name")
+	//	age := c.Param("age")
+	//
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"name": name,
+	//		"age":  age,
+	//	})
+	//})
+
+	r.GET("/blog/:year/:month", func(c *gin.Context) {
+		year := c.Param("year")
+		month := c.Param("month")
+
+		c.JSON(http.StatusOK, gin.H{
+			"year":  year,
+			"month": month,
+		})
+	})
+
+	r.Run(":9000")
+}
